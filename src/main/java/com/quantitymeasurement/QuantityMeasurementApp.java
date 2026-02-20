107a524737441640665a71d4b5edcd34aa999fc5
Muskan gupta <muskangupta15223@gmail.com>
2026-02-20 17:17:19 +0530

    [ Muskan Gupta ] : commited quantityMeasurement InchesEquality java program

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,11 +2,22 @@ package com.quantitymeasurement;
 
 public class QuantityMeasurementApp {
 
+	public static void demonstrateFeetEquality() {
+
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(1.0);
+
+		System.out.println(" Check Feet Equal: " + f1.equals(f2));
+	}
+	
+	public static void demonstrateInchesEquality() {
+		Inches i1 = new Inches(1.0);
+		Inches i2 = new Inches(2.0);
+		System.out.println("Check Inches Equal : " + i1.equals(i2));
+	}
 	public static void main(String[] args) {
-
-			Feet f1 = new Feet(1.0);
-			Feet f2 = new Feet(1.0);
-
-			System.out.println("Equal: " + f1.equals(f2));
+		
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
 	}
 }
