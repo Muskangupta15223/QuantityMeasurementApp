98c37bba36f38128eb3219b47bf721b65549b59c
Muskan gupta <muskangupta15223@gmail.com>
2026-02-25 10:48:32 +0530

    [ Muskan Gupta ] : commited quantityMeasurement UC8-StandaloneUnitTest java program

--- src/test/java/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -2,80 +2,11 @@ package quantitymeasurement;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import com.quantitymeasurement.Feet;
-import com.quantitymeasurement.Inches;
 import com.quantitymeasurement.Length;
 import com.quantitymeasurement.LengthUnit;
 import com.quantitymeasurement.QuantityMeasurementApp;
 
 public class QuantityMeasurementAppTest {
-
-	@Test
-	void testFeetEquality_SameValue() {
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
-		assertTrue(f1.equals(f2));
-	}
-
-	@Test
-	void testFeetEquality_DifferentValue() {
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(2.0);
-		assertFalse(f1.equals(f2));
-	}
-
-	@Test
-	void testFeetEquality_NullComparison() {
-		Feet f1 = new Feet(1.0);
-		assertFalse(f1.equals(null));
-	}
-
-	@Test
-	void testFeetEquality_DifferentClass() {
-		Feet f1 = new Feet(1.0);
-		String str = "1.0";
-		assertFalse(f1.equals(str));
-	}
-
-	@Test
-	void testFeetEquality_SameReference() {
-		Feet f1 = new Feet(1.0);
-		assertTrue(f1.equals(f1));
-	}
-
-	@Test
-	void testInchesEquality_SameValue() {
-		Inches i1 = new Inches(1.0);
-		Inches i2 = new Inches(1.0);
-		assertTrue(i1.equals(i2));
-	}
-
-	@Test
-	void testInchesEquality_DifferentValue() {
-		Inches i1 = new Inches(1.0);
-		Inches i2 = new Inches(2.0);
-		assertFalse(i1.equals(i2));
-	}
-
-	@Test
-	void testInchesEquality_NullComparison() {
-		Inches i1 = new Inches(1.0);
-		assertFalse(i1.equals(null));
-	}
-
-	@Test
-	void testInchesEquality_DifferentClass() {
-		Inches f1 = new Inches(1.0);
-		String str = "1.0";
-		assertFalse(f1.equals(str));
-	}
-
-	@Test
-	void testInchesEquality_SameReference() {
-		Inches i1 = new Inches(1.0);
-		assertTrue(i1.equals(i1));
-	}
-
 	@Test
 	void testEquality_FeetToFeet_SameValue() {
 		Length l1 = new Length(1.0, LengthUnit.Feet);
