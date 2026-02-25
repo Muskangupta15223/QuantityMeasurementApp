41e0b533a25b3cc89d27e9e8a91a6d60ca5f3bd3
Muskan gupta <muskangupta15223@gmail.com>
2026-02-25 10:40:28 +0530

    [ Muskan Gupta ] : commited quantityMeasurement UC7-TargetUnitAddition java program

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,19 +1,6 @@
 package com.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-
-	public static void demonstrateFeetEquality() {
-		Feet f1 = new Feet(1.0);
-		Feet f2 = new Feet(1.0);
-		System.out.println("Check Feet Equal: " + f1.equals(f2));
-	}
-
-	public static void demonstrateInchesEquality() {
-		Inches i1 = new Inches(1.0);
-		Inches i2 = new Inches(2.0);
-		System.out.println("Check Inches Equal : " + i1.equals(i2));
-	}
-
 	public static boolean demonstrateLengthEquality(Length l1, Length l2) {
 		return l1.equals(l2);
 	}
@@ -60,8 +47,6 @@ public class QuantityMeasurementApp {
 	}
 	
 	public static void main(String[] args) {
-		demonstrateFeetEquality();
-		demonstrateInchesEquality();
 		demonstrateFeetInchesComparison();
 
 		demonstrateLengthComparison(1.0, Length.LengthUnit.Feet, 12.0, Length.LengthUnit.Inches);
