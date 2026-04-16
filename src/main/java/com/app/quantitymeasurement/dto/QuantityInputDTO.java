12681910efaae19218e8bee6eddb7621e41126f7
Muskan gupta <muskangupta15223@gmail.com>
2026-04-16 20:19:47 +0530

    [Muskan gupta] : commit UC18 and initial setup files of docker

--- src/main/java/com/app/quantitymeasurement/dto/QuantityInputDTO.java
+++ src/main/java/com/app/quantitymeasurement/dto/QuantityInputDTO.java
@@ -3,7 +3,6 @@ package com.app.quantitymeasurement.dto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
